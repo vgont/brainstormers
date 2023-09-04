4db2b447d778f3306a8861e6339b2c4e21828964
vgont <vgontijo34@gmail.com>
2023-09-04 11:31:08 -0300

    test Dropdown component

--- src/app/components/Dropdown.js
+++ src/app/components/Dropdown.js
@@ -10,7 +10,7 @@ export default function Dropdown() {
     return (
         <div className="relative flex flex-col items-center w-[340px] h-[340px] rounded-lg">
             <button onClick={()=> setIsOpen((prev)=> !prev)} className="bg-blue-400 p-4 w-full flex items-center justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white">
-                Dropdown
+                Tempo de uso
                 {!isOpen ? (
                     <AiFillCaretDown className="h-8"/>
                 ): (
@@ -21,8 +21,8 @@ export default function Dropdown() {
             {isOpen && (
                 <div className="bg-blue-400 absolute top-20 flex flex-col items-start round-lg p-2 w-full" id="combobox">
                     {ageList.map((item)=> (
-                        <div key={item.age} className="flex w-full justify-center hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4">
-                            <button className="font-bold  p-4 ">{item.age}{item.year}</button>
+                        <div key={item.age} className="flex w-full hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4">
+                            <button className="font-bold  p-4">{item.age}  {item.year}</button>
                             <button className="font-bold  p-4"></button>
                         </div>
                     ))}                   
