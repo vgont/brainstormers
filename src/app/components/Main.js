a76c725eed6d3ce01dc0d33b5ca40db2b60bf202
vgont <vgontijo34@gmail.com>
2023-09-03 22:56:53 -0300

    fixed components

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,6 +1,6 @@
 export default function Main({children}){
     return(
-        <main className="flex flex-col items-center my-16 mx-auto">
+        <main className="flex flex-col items-center my-16 mx-auto gap-10">
             {children}
         </main>
     )
--- src/app/components/Toggle.js
+++ src/app/components/Toggle.js
@@ -1,16 +1,17 @@
 "use client";
 import { useState } from "react";
 
-export default function Toggle(){
-    const [toggle, setToggle] = useState();
+export default function Toggle( { onToggle } ){
+    const [toggle, setToggle] = useState(false);
 
-    function changeToggle(e){
-        return setToggle(e.target.checked)
+    function changeToggle(){
+        setToggle(!toggle)
+        onToggle(toggle)
     }
 
     return(
         <label className="relative inline-flex items-center mb-5 cursor-pointer">
-        <input onChange={(e)=> changeToggle(e)} type="checkbox" value="" className="sr-only peer"/>
+        <input onClick={changeToggle} type="checkbox" value="" className="sr-only peer"/>
         <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
         <span className="ml-3 text-sm font-medium">{toggle? 'Sim':'Não'}</span>
         </label>
