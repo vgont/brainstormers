635dba353b1d65ee762a2be74e695707ea4630bf
vgont <vgontijo34@gmail.com>
2023-09-03 22:57:44 -0300

    started Input component

---
+++ src/app/components/Input.js
@@ -0,0 +1,10 @@
+export default function Input(){
+    return(
+
+        <form className="flex flex-col items-center justify-center">
+            <label className="font-semibold" for="fpotencia">Potência</label> <br/>
+            <input name="fpotencia" placeholder="exemple: 250"/>
+        </form>
+
+    )
+}
