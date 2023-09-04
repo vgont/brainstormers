ea38134714c81f875775353405da340822743dee
vgont <vgontijo34@gmail.com>
2023-09-04 10:09:26 -0300

    fixed footer link

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer({hideArrowBack,link}){
     return(
         <div className="w-full h-20 bg-hf-blue flex flex-row items-center p-3 justify-between fixed bottom-0">
             {
-                !hideArrowBack && <LinkArrow left={true} link={"/tipo"} texto={'VOLTAR'}/>
+                !hideArrowBack && <LinkArrow left={true} link={"./"} texto={'VOLTAR'}/>
             }
             <LinkArrow link={link} texto={'AVANÇAR'}/>
         </div>
