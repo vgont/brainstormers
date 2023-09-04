78fbaecafeb29f21ed905e6ca99e7d55fe1feab8
vgont <vgontijo34@gmail.com>
2023-09-03 22:56:33 -0300

    add Dropdown component

--- src/app/components/Dropdown.js
+++ src/app/components/Dropdown.js
@@ -1,26 +1,36 @@
+"use client";
+import React, { useState } from "react";
+import { AiFillCaretUp, AiFillCaretDown } from "react-icons/ai";
+import ageList from "@/app/ageList"
+
 export default function Dropdown() {
+
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div>
-            <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
-            </svg></button>
-            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
-                    </li>
-                </ul>
-            </div>
+        <div className="relative flex flex-col items-center w-[340px] h-[340px] rounded-lg">
+            <button onClick={()=> setIsOpen((prev)=> !prev)} className="bg-blue-400 p-4 w-full flex items-center justify-between font-bold text-lg rounded-lg tracking-wider border-4 border-transparent active:border-white duration-300 active:text-white">
+                Dropdown
+                {!isOpen ? (
+                    <AiFillCaretDown className="h-8"/>
+                ): (
+                    <AiFillCaretUp className="h-8"/>
+                )
+                }
+            </button>
+            {isOpen && (
+                <div className="bg-blue-400 absolute top-20 flex flex-col items-start round-lg p-2 w-full">
+                    {ageList.map((item, i )=> (
+                        <div className="flex w-full justify-center hover:bg-blue-300 cursor-pointer rounded-r-lg border-l-transparent hover:border-l-white border-l-4">
+                            <button className="font-bold  p-4 ">{item.age}{item.year}</button>
+                            <button className="font-bold  p-4"></button>
+                        </div>
+                    ))}                   
+                </div>
+            )}
 
         </div>
     )
 }
+
+// className="relative flex flex-col items-center w-[340px] h-[340px] rounded-lg"
