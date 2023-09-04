2c5404e11d0ccb414adc9dc99bf1283d0ad112c5
vgont <vgontijo34@gmail.com>
2023-09-04 10:10:14 -0300

    structure of Input component modified

--- src/app/components/Input.js
+++ src/app/components/Input.js
@@ -1,10 +1,41 @@
-export default function Input(){
-    return(
+import { useState } from "react";
 
-        <form className="flex flex-col items-center justify-center">
-            <label className="font-semibold" for="fpotencia">Potência</label> <br/>
-            <input name="fpotencia" placeholder="exemple: 250"/>
-        </form>
+export default function Input({ texto, submitValue, pHolder, valueLimit }) {
+
+    const [fpotencia, setFpotencia] = useState('');
+    const [error, setError] = useState('');
+
+    const handleInputChange = e => {
+        setFpotencia(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (fpotencia === '') {
+            setError('Name must be filled out');
+        } else {
+            console.log('Form submitted:', fpotencia);
+            setFpotencia('');
+            setError('');
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
+                <label className="font-semibold" name="fpotencia" onChange={handleInputChange}>Potência</label> <br />
+                <div className="flex mb-6">
+                    <input name="watts" type="text " placeholder={pHolder} className="w-24 h-10 flex-1 px-3 py-2 border-l-2 border-y-2 border-sky-950" />
+                    <span className="h-10 flex-none px-3 py-2 bg-opacity-10 bg-gray-400 text-gray-600 rounded-r border-2 border-sky-950">{texto}</span>
+                </div>
+                <input type="submit" value={submitValue} />
+            </form>
+            <p className="m-10 font-semibold">
+                <span className="text-red-500">! </span>
+                {valueLimit}
+            </p>
+        </div>
 
     )
 }
