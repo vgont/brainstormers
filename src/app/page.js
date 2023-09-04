8240f13c4b580a748e69320a9cbda00c15d4600b
vgont <vgontijo34@gmail.com>
2023-09-04 12:57:41 -0300

    order of buttons changed

--- src/app/page.js
+++ src/app/page.js
@@ -7,15 +7,15 @@ import Main from "./components/Main";
 export default function Home() {
   return (
     <div>
-      <Header hideHomeButton/>
+      <Header hideHomeButton />
       <Main>
-        <Image className="mb-32" src={'/images/porto.png'} width={'284'} height={'134'}/>
+        <Image className="mb-10" src={'/images/porto.png'} width={'284'} height={'134'} />
         <h2 className='text-lg font-Roboto font-bold'>Seja bem vindo!</h2>
         <div className="mt-24 w-11/12 flex justify-evenly flex-wrap max-w-mw gap-7">
-          <Button className={'bg-gray-button text-black'} texto={'Ver minhas bikes'}/>
           <Link href={"/tipo"}>
-          <Button className={'bg-blue-button text-white'} texto={'Cadastre sua bike'}/>
+            <Button className={'bg-blue-button text-white'} texto={'Cadastre sua bike'} />
           </Link>
+          <Button className={'bg-gray-button text-black'} texto={'Ver minhas bikes'} />
         </div>
       </Main>
     </div>
