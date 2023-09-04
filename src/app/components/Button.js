f3be0c3605e4bcc761f4755e8cd941f204502715
vgont <vgontijo34@gmail.com>
2023-09-04 12:58:34 -0300

    button width fixed

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Button({className, texto}){
     return( 
-        <button className={`flex flex-col w-40 h-16 p-5 justify-center items-center rounded-lg font-semibold ${className}`}>
+        <button className={`flex flex-col w-52 h-16 p-5 justify-center items-center rounded-lg font-semibold ${className}`}>
             {texto}
         </button>
     )
