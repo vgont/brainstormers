678597fafc80816e9a8876acf2cf575c45b92daf
vgont <vgontijo34@gmail.com>
2023-09-05 16:40:27 -0300

    add useState

--- src/app/tipo/potencia/modificacoes/page.js
+++ src/app/tipo/potencia/modificacoes/page.js
@@ -1,21 +1,29 @@
+"use client";
 import Footer from "@/app/components/Footer";
 import Header from "@/app/components/Header";
 import Main from "@/app/components/Main";
 import Title from "@/app/components/Title";
 import Toggle from "@/app/components/Toggle";
+import { useState } from "react";
 
 
 export default function Modificacoes() {
+  
+  const [modificacoes, setModificacoes] = useState(false);
+  const [acessorios, setAcessorios] = useState(false);
+
   return (
     <div>
-      <Header />
+      <Header/>
       <Main>
-        <Title texto={'Sua bike possui modificações?'}/>
-        <Toggle/>
+        <Title texto={'Sua bike possui modificações?'} />
+        <Toggle onToggle={setModificacoes}/>
         <Title texto={'Sua bike possui acessórios?'}/>
-        <Toggle/>
+        <Toggle onToggle={setAcessorios}/>
       </Main>
-      <Footer link={"/tipo/potencia/modificacoes/comprovantes"} />
+      <Footer link={
+        modificacoes || acessorios? "/tipo/potencia/modificacoes/comprovante_modificacoes":"/tipo/potencia/modificacoes/comprovantes"
+      }/>
     </div>
   )
 }
