2ad7a52893064f80699246550f4e346eb2547674
vgont <vgontijo34@gmail.com>
2023-09-05 14:19:27 -0300

    add another ?: in Button component

--- src/app/components/Button.js
+++ src/app/components/Button.js
@@ -1,10 +1,15 @@
+import Image from "next/image";
 import Link from "next/link";
 
-export default function Button({texto, blueColor }) {
+export default function Button({texto, blueColor, photoButton}) {
     return (
-        <button className={`flex flex-col w-52 h-16 p-5 justify-center items-center rounded-lg font-semibold 
-        ${!blueColor? 'bg-gray-button text-black': 'bg-blue-button text-white'}`}>
+        <button className={
+            `flex flex-row h-14 p-5 justify-center items-center rounded-lg font-semibold 
+            ${photoButton? 'w-60 gap-5':'w-52'} 
+            ${blueColor? 'bg-blue-button text-white':'bg-gray-button text-black'}`
+        }>
             {texto}
+            {photoButton && <Image className="mx-auto" src={'/images/camera.png'} width={31} height={27}/>}
         </button>
     )
 }
