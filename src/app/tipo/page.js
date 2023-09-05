23f1cc9d0c0bfd98fb56e24db688ae868a801592
vgont <vgontijo34@gmail.com>
2023-09-04 21:20:40 -0300

    remove BikeType component

--- src/app/tipo/page.js
+++ src/app/tipo/page.js
@@ -2,7 +2,7 @@ import Header from "../components/Header";
 import Title from "../components/Title";
 import Footer from "../components/Footer";
 import Main from "../components/Main";
-import BikeType from "../components/BikeType";
+import Button from "../components/Button";
 
 export default function Tipo() {
   return (
@@ -11,15 +11,9 @@ export default function Tipo() {
       <Main>
         <div className="flex items-center justify-center flex-col w-11/12 gap-12">
           <Title texto={'Qual o tipo da sua bike?'} />
-          <BikeType
-            tipo={'Performance'}
-          />
-          <BikeType
-            tipo={'Mountain Bike'}
-          />
-          <BikeType
-            tipo={'Urbana'}
-          />
+          <Button texto={'Performance'} className={'bg-gray-button text-black'}/>  
+          <Button texto={'Mountain Bike'} className={'bg-gray-button text-black'}/>   
+          <Button texto={'Urbana'} className={'bg-gray-button text-black'}/>   
         </div>
       </Main>
       <Footer hideArrowBack={true} link={"/tipo/potencia"} />
