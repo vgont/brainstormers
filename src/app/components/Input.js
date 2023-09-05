4457cd98531fbf4c43a5b2af1b016825c730e9d8
vgont <vgontijo34@gmail.com>
2023-09-05 16:39:08 -0300

    put max in input

--- src/app/components/Input.js
+++ src/app/components/Input.js
@@ -26,10 +26,10 @@ export default function Input({ texto, submitValue, pHolder, valueLimit }) {
             <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center">
                 <label className="font-semibold" name="fpotencia" onChange={handleInputChange}>Potência</label> <br />
                 <div className="flex mb-6">
-                    <input name="watts" type="text " placeholder={pHolder} className="w-24 h-10 flex-1 px-3 py-2 border-l-2 border-y-2 border-sky-950" />
+                    <input max={250} name="watts" type="text " placeholder={pHolder} className="w-24 h-10 flex-1 px-3 py-2 border-l-2 border-y-2 border-sky-950" />
                     <span className="h-10 flex-none px-3 py-2 bg-opacity-10 bg-gray-400 text-gray-600 rounded-r border-2 border-sky-950">{texto}</span>
                 </div>
-                <input type="submit" value={submitValue} />
+                <button type="submit" value={submitValue}>Verificar Potência</button>
             </form>
             <p className="m-10 font-semibold">
                 <span className="text-red-500">! </span>
